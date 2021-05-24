e639f1b612561e9e9f01b6736bf0b4190ce77ccd
ethanmac <ethanmac@mac.com>
2021-05-24 22:24:11 +0800

    update

--- utils.py
+++ utils.py
@@ -1,4 +1,5 @@
 import resnet_cifar10
+import resnet_cifar100
 import tensorflow as tf
 import matplotlib.pyplot as plt
 
@@ -21,6 +22,29 @@ def get_training_model():
 
     # The Classifier for 10 classes
     outputs = resnet_cifar10.classifier(x, 10)
+
+    # Instantiate the Model
+    model = tf.keras.Model(inputs, outputs)
+    
+    return model
+
+def get_training_model_100():
+    # ResNet20
+    n = 2
+    depth =  n * 9 + 2
+    n_blocks = ((depth - 2) // 9) - 1
+
+    # The input tensor
+    inputs = tf.keras.layers.Input(shape=(32, 32, 3))
+
+    # The Stem Convolution Group
+    x = resnet_cifar100.stem(inputs)
+
+    # The learner
+    x = resnet_cifar100.learner(x, n_blocks)
+
+    # The Classifier for 100 classes
+    outputs = resnet_cifar100.classifier(x, 100)
 
     # Instantiate the Model
     model = tf.keras.Model(inputs, outputs)
